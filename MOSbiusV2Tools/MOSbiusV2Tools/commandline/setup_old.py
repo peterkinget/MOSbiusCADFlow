70ea1978ca8e2a3edd0c759ac4c62d2b26ad3872
Peter Kinget <peter@kinget.net>
2025-05-09 12:08:06 -0400

    quick update setup.py

---
+++ MOSbiusV2Tools/MOSbiusV2Tools/commandline/setup_old.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="MOSbiusV2Tools",
+    version="1.0.0",
+    description="Tools for generating SPICE subcircuits from JSON circuit descriptions and sizing files.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    author="Peter Kinget",
+    author_email="peter@kinget.net",
+    url="https://github.com/peterkinget/MOSbiusV2Tools",
+    license="MIT",
+    packages=find_packages(include=["commandline", "commandline.src"]),
+    install_requires=open("commandline/requirements.txt").read().splitlines(),
+    entry_points={
+        "console_scripts": [
+            "generate_sizes_probe_subckt=commandline.src.generate_sizes_probe_subckt:main",
+            "generate_switch_matrix_probe_subckt=commandline.src.generate_switch_matrix_probe_subckt:main",
+            "generate_pin_to_RBUS_SBUS_subckt=commandline.src.generate_pin_to_RBUS_SBUS_subckt:main",
+            "generate_nodes_to_subckt=commandline.src.generate_nodes_to_subckt:main",
+        ],
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
+
